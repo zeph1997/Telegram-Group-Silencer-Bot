10afb15333bc5fd3002df326508239514777a46d
zeph1997 <zeph1997@hotmail.com>
2020-07-23 10:37:35 +0800

    add checks if the bot is the one being removed

--- main.py
+++ main.py
@@ -4,12 +4,24 @@ TOKEN = "Your Telegram API Token"
 
 bot = telebot.TeleBot(TOKEN)
 
-@bot.message_handler(content_types=['new_chat_members','left_chat_member'])
-def delete_join_leave_message(m):
+@bot.message_handler(content_types=['new_chat_members'])
+def delete_join_message(m):
     # If bot is not admin, then it will not be able to delete message.
     try:
         bot.delete_message(m.chat.id,m.message_id)
     except:
-        bot.send_message(m.chat.id, "Please make me an admin in order for me to remove the join and leave messages on this group!")
+        if m.new_chat_member.id != bot.get_me().id:
+            bot.send_message(m.chat.id,"Please make me an admin in order for me to remove the join and leave messages on this group!")
+        else:
+            bot.send_message(m.chat.id,"Hi! I am your trusty GroupSilencer Bot! Thanks for adding me! To use me, make me an admin and I will be able to delete all the pesky notification when a member joins or leaves the group!")
         
+@bot.message_handler(content_types=['left_chat_member'])
+def delete_leave_message(m):
+    # If bot is the one that is being removed, it will not be able to delete the leave message.
+    if m.left_chat_member.id != bot.get_me().id:
+        try:
+            bot.delete_message(m.chat.id,m.message_id)
+        except:
+            bot.send_message(m.chat.id,"Please make me an admin in order for me to remove the join and leave messages on this group!")
+
 bot.polling()
